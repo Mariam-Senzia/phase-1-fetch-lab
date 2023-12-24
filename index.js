613d1aad7669f7db038060baf0d931c1ca339ada
Mariam Senzia <mariamsenzia@gmail.com>
2023-12-24 09:01:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function fetchBooks() {
 
 }
 
-function renderBooks(json) {
+function renderBooks(books) {
   const main = document.querySelector('main');
-  json.forEach(book => {
+  books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
